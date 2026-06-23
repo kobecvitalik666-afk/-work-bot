84bd3408b5852a015de6c4d142569ede26a66215
kobecvitalik666-afk <kobecvitalik666@gmail.com>
2026-06-24 01:07:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
 
 TOKEN = Your token was replaced with a new one. You can use this token to access HTTP API:
 8910960847:AAFCo7Br7cbgpmK77mRAONcUNDpXZczvb2c
@@ -7,11 +9,16 @@ TOKEN = Your token was replaced with a new one. You can use this token to access
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+@dp.message(Command("start"))
+async def start(msg: Message):
+    await msg.answer("Бот работает")
+
 @dp.message()
-async def handler(msg: types.Message):
-    await msg.answer("Бот работает ✅")
+async def echo(msg: Message):
+    await msg.answer(msg.text)
 
 async def main():
     await dp.start_polling(bot)
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
